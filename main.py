98a62c720892104a2f1171b11abeb75b57ab6bea
Johnatan <johnatan.f@hotmail.com>
2024-08-07 17:13:27 +0200

    Day 1 python bootcamp commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# print("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in? ")
+# city = input()
+# print("What's your pet's name?")
+# pet = input()
+# print("Your band name could be " + city + " " + pet + "!")
+
+print("Welcome to the Band Name Generator!!")
+food = input("What is your least favorite food? ")
+drink = input("What is your favorite drink? ")
+print("Your band name could be " + food + " 'n " + drink + "!")
